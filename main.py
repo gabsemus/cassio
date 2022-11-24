1223279c199ac6b9afbf9973e86d13db345ac1ef
gabsemus <88344550+gabsemus@users.noreply.github.com>
2022-11-24 14:57:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ from pytz import timezone
 
 import random
 
-data_hora = datetime.now()
-fuso_horario = timezone('America/Sao_Paulo')
-data_hora = data_hora.astimezone(fuso_horario)
-data = data_hora.strftime('%d')
-hora = data_hora.strftime('%H:%M')
+def data_hora():
+   data_hora = datetime.now()
+   fuso_horario = timezone('America/Sao_Paulo')
+   data_hora = data_hora.astimezone(fuso_horario)
+   data = data_hora.strftime('%d')
+   # hora = data_hora.strftime('%H:%M')
+   return data
 
 def home():
-
 
    nome_local = 'Kingdom'
    st.title(f'Bem-vindo a {nome_local}')
 
-   st.subheader(f"Hoje, dia {data}, temos as seguintes atrações: ")
+   st.subheader(f"Hoje, dia {data_hora()}, temos as seguintes atrações: ")
 
    line = ['DJ Sande', 'DJ Stizi', 'DJ Aron Kawillian', 'DJ Gouveia']
 
-   emoji = [':full_moon:', ':new_moon_with_face:', ':full_moon_with_face:', ':headphones:', ':hatched_chick:', ':lips:', ':fire:', '	:firecracker:']
-
-
+   emoji = [':full_moon:', ':new_moon_with_face:', ':full_moon_with_face:', ':headphones:', ':hatched_chick:', ':lips:', ':fire:']
 
    for dj in line:
       rand_idx = random.randrange(len(emoji))
@@ -45,8 +44,24 @@ def home():
       with col2:
          st.markdown(f'{promo[produto]}')
 
+   st.subheader(f" :fire: Informações da Casa: :fire: ")
 
-
+   st.markdown(
+      """
+  <style>
+  [data-testid="stMetricLabel"] {
+      font-size: 25px;
+  }
+  [data-testid="stMetricValue"] {
+      font-size: 50px;
+  }
+  
+  </style>
+  
+  """,
+      unsafe_allow_html=True,
+   )
+   st.metric(label="Lotação da Casa", value=4)
 
 def cadastro_usuario():
    st.subheader('Cadastro do Cliente')
